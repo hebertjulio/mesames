5368f0938b2b9f39a1c98883691fc3fb74e481e8
Hebert <hebertjulio@yahoo.com.br>
2021-07-05 10:38:10 -0300

    Ajustes na bottombar e no layout do componente chart by category

--- lib/components/bottom_navigator.dart
+++ lib/components/bottom_navigator.dart
@@ -3,49 +3,29 @@ import 'package:flutter/material.dart';
 class MonthlyBottomNavigator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
+    return BottomAppBar(
+      color: Colors.blue,
+      shape: CircularNotchedRectangle(),
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        padding: const EdgeInsets.only(left: 16.0, right: 16.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Row(
-              children: [
-                IconButton(
-                  icon: Icon(
-                    Icons.arrow_back,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {},
-                ),
-                Text(
-                  'Jan/2021',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
+          children: [
+            ElevatedButton.icon(
+              label: Text('Jan/2021'),
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+              ),
+              onPressed: () {},
             ),
-            SizedBox.shrink(),
-            Row(
-              children: [
-                Text(
-                  'Mar/2021',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(
-                    Icons.arrow_forward,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {},
-                ),
-              ],
+            ElevatedButton.icon(
+              icon: Text('Mar/2021'),
+              label: Icon(
+                Icons.arrow_forward,
+                color: Colors.white,
+              ),
+              onPressed: () {},
             ),
           ],
         ),
