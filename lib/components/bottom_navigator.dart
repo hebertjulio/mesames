b2f297a088096e278365d404ab6376ec587e9353
Hebert <hebertjulio@yahoo.com.br>
2021-08-06 17:25:16 -0300

    Bottom app navigation refactory

--- lib/components/bottom_navigator.dart
+++ lib/components/bottom_navigator.dart
@@ -6,29 +6,69 @@ class MonthlyBottomNavigator extends StatelessWidget {
     return BottomAppBar(
       color: Colors.blue,
       shape: CircularNotchedRectangle(),
-      child: Padding(
-        padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            ElevatedButton.icon(
-              label: Text('Jan/2021'),
-              icon: Icon(
-                Icons.arrow_back,
-                color: Colors.white,
-              ),
-              onPressed: () {},
-            ),
-            ElevatedButton.icon(
-              icon: Text('Mar/2021'),
-              label: Icon(
-                Icons.arrow_forward,
-                color: Colors.white,
-              ),
-              onPressed: () {},
-            ),
-          ],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          MonthlyButtonBottomNavigator(
+            'Jan/2021',
+            reversed: false,
+            onTap: () {},
+          ),
+          MonthlyButtonBottomNavigator(
+            'Mar/2021',
+            reversed: true,
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MonthlyButtonBottomNavigator extends StatelessWidget {
+  final String text;
+  final Function onTap;
+  final bool reversed;
+
+  MonthlyButtonBottomNavigator(this.text, {@required this.onTap, this.reversed})
+      : assert(text != null);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> children = [
+      Padding(
+        padding: EdgeInsets.only(left: 8.0, right: 8.0),
+        child: Icon(
+          this.reversed ? Icons.arrow_forward : Icons.arrow_back,
+          color: Colors.white,
         ),
+      ),
+      Container(
+        width: 100,
+        child: Center(
+          child: Text(
+            this.text,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
+    ];
+
+    return Expanded(
+      child: InkWell(
+        child: Ink(
+          height: Size.fromHeight(50).height,
+          child: Row(
+            mainAxisAlignment:
+                this.reversed ? MainAxisAlignment.end : MainAxisAlignment.start,
+            children: this.reversed ? children.reversed.toList() : children,
+          ),
+        ),
+        onTap: this.onTap,
       ),
     );
   }
